6e0e266425b9ca43e01824697ed73c335a43f940
brianliao1999 <49636110+brianliao1999@users.noreply.github.com>
2020-05-23 18:37:20 +0800

    Add files via upload

--- TM_TotalALU_test.v
+++ TM_TotalALU_test.v
@@ -9,6 +9,29 @@ module TM_TotalALU_test ;
   
   initial begin
     #t // and
+    signal = 3'b000 ;
+    a = 4'b0011 ;
+    b = 4'b1101 ;
+
+    #t // add
+      signal = 3'b010 ;
+      a = 4'b0010 ;
+      b = 4'b1100 ;
+	  
+    #t 
+      a = 4'b1000 ;
+      b = 4'b1111 ;
+
+    #t // sub 
+      signal = 3'b110 ;
+      a = 4'b1011 ;
+      b = 4'b0010 ;
+
+    #t 	
+      a = 4'b0110 ;
+      b = 4'b0111 ;
+    
+    #t // and
       signal = 3'b000 ;
       a = 4'b1110 ;
       b = 4'b0101 ;	
@@ -20,37 +43,11 @@ module TM_TotalALU_test ;
     #t 	
       a = 4'b0110 ;
       b = 4'b0100 ;
-
+  
     #t // or
       signal = 3'b001 ;
       a = 4'b1011 ;
       b = 4'b0000 ;
-    
-    #t
-      a = 4'b1010 ;
-      b = 4'b1000 ;
-	 
-    #t // add
-      signal = 3'b010 ;
-      a = 4'b0010 ;
-      b = 4'b1100 ;
-	  
-    #t 
-      a = 4'b1000 ;
-      b = 4'b1111 ;
-
-    #t 
-      a = 4'b1000 ;
-      b = 4'b0001 ;
-
-    #t // sub 
-      signal = 3'b110 ;
-      a = 4'b1011 ;
-      b = 4'b0010 ;
-
-    #t 	
-      a = 4'b0110 ;
-      b = 4'b0111 ;
 	  
     #t // slt
       signal = 3'b111 ;
@@ -58,6 +55,6 @@ module TM_TotalALU_test ;
       b = 4'b0101 ;	 
 	  
     #t 
-	  $stop;
+      $stop;
   end 
 endmodule 
